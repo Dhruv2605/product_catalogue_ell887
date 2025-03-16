382f5bf1d58deb8fedad4dd0c8a232d15b1a4822
Dhruv B <dhruv.bhogaonkar2605@gmail.com>
2025-03-16 15:54:31 +0530

    delete

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def clear_products():
 
         deleted_products = []
         for item in items:
-            partition_key = item["id"]  # Use 'id' as partition key
+            partition_key = item["name"]  # Use 'id' as partition key
             container.delete_item(item["id"], partition_key=partition_key)
             deleted_products.append(item["name"])
             logging.info(f"🗑️ Deleted product: {item['name']} (ID: {item['id']})")
